a35774b28936eb67edb6853f724970bba0a68ee5
kimbokChi <han15050424@gmail.com>
2021-04-19 16:03:20 +0900

    프로그래머스 : 코딩테스트 연습>스택/큐>주식가격 문제를 해결한다
    문제 지문 해석하는거 어려운데스

---
+++ Native_CSharp/CodingTest/Stack_Queue_02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Native_CSharp.CodingTest
+{
+    class Stack_Queue_02
+    {
+        public int[] Solution(int[] prices)
+        {
+            int index = 0;
+            var result = new Queue<int>();
+            
+            while (index < prices.Length)
+            {
+                int distance = 0;
+                for (int i = index + 1; i < prices.Length; i++)
+                {
+                    distance++;
+
+                    if (prices[index] > prices[i])
+                        break;
+                }
+                result.Enqueue(distance);
+                index++;
+            }
+            int[] answer = new int[prices.Length];
+
+            for (int i = 0; i < prices.Length; i++)
+                answer[i] = result.Dequeue();
+
+            return answer;
+        }
+    }
+}
