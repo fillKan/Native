acd52f94600c212f5d5a39b886760b7c374d951e
kimbokChi <han15050424@gmail.com>
2021-04-19 20:25:51 +0900

    프로그래머스 : 코딩테스트 연습>스택/큐>프린터 문제를 해결한다
    후 좀 어려웠다 이번에는 실행 코드도 첨가함~

---
+++ Native_CSharp/CodingTest/Stack_Queue_03.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Native_CSharp.CodingTest
+{
+    class Stack_Queue_03
+    {
+        public int Solution(int[] priorities, int location)
+        {
+            int answer = 0;
+            Queue<int> values = new Queue<int>(priorities);
+
+            int[] temp = new int[priorities.Length];
+            priorities.CopyTo(temp, 0);
+            
+            //
+            for (int i = 0; i < temp.Length; i++)
+            {
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (temp[i] > temp[j])
+                    {
+                        int t = temp[i];
+                        temp[i] = temp[j];
+                        temp[j] = t;
+                    }
+                }
+            }
+            //
+            Queue<int> rank = new Queue<int>(temp);
+            // 1, 1, 9, 1, 1, 1
+            while (rank.Count > 0)
+            {
+                if (rank.Peek() != values.Peek())
+                {
+                    values.Enqueue(values.Dequeue());
+
+                    if (location == 0) {
+                        location = values.Count - 1;
+                    }
+                    else
+                        location--;
+                }
+                else
+                {
+                    rank.Dequeue();
+                    values.Dequeue();
+
+                    answer++;
+
+                    if (location == 0) {
+                        break;
+                    }
+                    location--;
+                }
+            }
+            return answer;
+        }
+    }
+}
--- Native_CSharp/Program.cs
+++ Native_CSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Native_CSharp.LinkedList;
+using Native_CSharp.CodingTest;
 
 namespace Native_CSharp
 {
@@ -8,6 +9,9 @@ namespace Native_CSharp
     {
         static void Main(string[] args)
         {
+            var sq = new Stack_Queue_03();
+
+            Console.WriteLine(sq.Solution(new int[6] { 1, 1, 9, 1, 1, 1 }, 0));
         }
     }
 }
